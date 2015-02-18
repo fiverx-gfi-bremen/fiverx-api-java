abe01bcb7bdedc2c9be6bea958c36849d1f76e1e
Captain-P-Goldfish <deedlit1702>
2015-02-18 18:06:39 +0100

    ein signinghelper interface und eine implementierung fuer rsa mit sha 1 signatur

---
+++ src/main/java/de/vsa/fiverx/crypto/xml/XmlSigningHelper.java
@@ -0,0 +1,19 @@
+package de.vsa.fiverx.crypto.xml;
+
+import org.apache.xml.security.exceptions.XMLSecurityException;
+import org.w3c.dom.Document;
+
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+
+/**
+ * Author: Pascal Knueppel Date: 18.02.2015 Time: 17:21
+ */
+public interface XmlSigningHelper {
+
+    public void addSignatureToDocument (Document document, PrivateKey signingKey, X509Certificate signingCertificate)
+            throws XMLSecurityException;
+
+    public boolean verifySignature (Document document, X509Certificate verficationCertificate)
+            throws XMLSecurityException;
+}
---
+++ src/main/java/de/vsa/fiverx/crypto/xml/XmlSigningHelperRsaSha1.java
@@ -0,0 +1,87 @@
+package de.vsa.fiverx.crypto.xml;
+
+import org.apache.xml.security.algorithms.MessageDigestAlgorithm;
+import org.apache.xml.security.c14n.Canonicalizer;
+import org.apache.xml.security.exceptions.XMLSecurityException;
+import org.apache.xml.security.keys.KeyInfo;
+import org.apache.xml.security.signature.XMLSignature;
+import org.apache.xml.security.transforms.Transforms;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.UUID;
+
+/**
+ * Author: Pascal Knueppel
+ * Date: 18.02.2015
+ * Time: 17:24
+ */
+public class XmlSigningHelperRsaSha1 implements XmlSigningHelper{
+    @Override
+    public void addSignatureToDocument (Document document, PrivateKey signingKey, X509Certificate signingCertificate)
+            throws XMLSecurityException {
+        String signatureAlgorithm = XMLSignature.ALGO_ID_SIGNATURE_RSA_SHA1;
+        String canonicalizationMethod = Canonicalizer.ALGO_ID_C14N_EXCL_OMIT_COMMENTS;
+
+        // create xmlSignature with RSA-SHA1 Signature and canonizalization without comments
+        XMLSignature xmlSignature = new XMLSignature( document,
+                "",
+                signatureAlgorithm,
+                canonicalizationMethod);
+        Element rootElement = document.getDocumentElement();
+        // add signature to root element of document
+
+        // The UUID is needed to reference the signature to the signed elements.
+        String id = UUID.randomUUID().toString();
+        rootElement.setAttributeNS(null, "sigId", id);
+        rootElement.setIdAttributeNS(null, "sigId", true);
+        Transforms transforms = new Transforms(document);
+        transforms.addTransform(canonicalizationMethod);
+        String messageDigestAlgorithm = MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA1;
+        // generate the digest values and them to the document
+        xmlSignature.addDocument("#" + id , transforms, messageDigestAlgorithm);
+        // sign the added digest values.
+        xmlSignature.sign(signingKey);
+        // append the signature to the document into the root-element.
+        rootElement.appendChild(xmlSignature.getElement());
+        // add the certificate to the signature so the signature can be verified. (does not provide real security of
+        // course)
+        xmlSignature.addKeyInfo(signingCertificate);
+    }
+
+    @Override
+    public boolean verifySignature (Document document, X509Certificate verficationCertificate)
+            throws XMLSecurityException {
+        XPathFactory xpf = XPathFactory.newInstance();
+        XPath xpath = xpf.newXPath();
+        xpath.setNamespaceContext(new DSNamespaceContext());
+        // Find the Signature Element
+        String expression = "//dsig:Signature[1]";
+        Element sigElement;
+        try {
+            sigElement = (Element) xpath.evaluate(expression, document, XPathConstants.NODE);
+        } catch (XPathExpressionException e) {
+            return false;
+        }
+        assert sigElement != null;
+
+        Element rootElement = document.getDocumentElement();
+        // do not build the hash over the signature element. Therefore remove it from the document.
+        rootElement.removeChild(sigElement);
+
+        XMLSignature xmlSignature = new XMLSignature(sigElement, "");
+
+        KeyInfo ki = xmlSignature.getKeyInfo();
+        if (ki == null) {
+            throw new XMLSecurityException("Keine KeyInfo und damit kein Zertifikat in der Signatur enthalten!");
+        }
+        // Check the Signature value
+        return xmlSignature.checkSignatureValue(verficationCertificate);
+    }
+}
