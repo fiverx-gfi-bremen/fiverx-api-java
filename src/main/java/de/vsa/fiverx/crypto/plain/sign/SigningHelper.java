094a44929189f4b328bde1282b9ba3c5911f0378
frito <frito.boggie@quantentunnel.de>
2015-02-19 08:30:46 +0100

    changed copyright notice

--- src/main/java/de/vsa/fiverx/crypto/plain/sign/SigningHelper.java
+++ src/main/java/de/vsa/fiverx/crypto/plain/sign/SigningHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 VSA Gmbh
+ * Copyright (c) 2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
