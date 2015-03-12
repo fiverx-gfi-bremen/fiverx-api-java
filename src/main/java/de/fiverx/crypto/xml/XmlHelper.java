63dd8c493f724e5cb658e3303dc1a2514251db0a
Captain-P-Goldfish <deedlit1702>
2015-03-12 15:23:03 +0100

    Ein paar Funktionen hinzugefuegt, zum hin und her wandeln von XML-Documents

--- src/main/java/de/fiverx/crypto/xml/XmlHelper.java
+++ src/main/java/de/fiverx/crypto/xml/XmlHelper.java
@@ -21,6 +21,7 @@ package de.fiverx.crypto.xml;
 
 import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -31,8 +32,10 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.StringWriter;
 
 /**
  * Kurzer Satz der die Klasse beschreibt.
@@ -60,6 +63,19 @@ public class XmlHelper {
 
         if (LOG.isInfoEnabled()) {
             LOG.info("retrieveXml: file=" + file + "; document=" + document + "; encoding=" + document.getXmlEncoding());
+        }
+
+        return document;
+    }
+
+    public static Document retrieveXml(String doc) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+        Document document = documentBuilder.parse(new ByteArrayInputStream(doc.getBytes()));
+
+        if (LOG.isInfoEnabled()) {
+            LOG.info("retrieveXml: doc=" + doc + "; document=" + document + "; encoding=" + document.getXmlEncoding());
         }
 
         return document;
@@ -108,5 +124,23 @@ public class XmlHelper {
         } catch (TransformerException e) {
             throw new IllegalStateException("Cloning dom failed!");
         }
+    }
+
+    public static String documentToString (Document document) throws TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(document), new StreamResult(writer));
+        return writer.getBuffer().toString().replaceAll("\n|\r", "");
+    }
+
+    public static String elementToString (Element element) throws TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(element), new StreamResult(writer));
+        return writer.getBuffer().toString().replaceAll("\n|\r", "");
     }
 }
